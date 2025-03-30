6e6b655b8051bf870a55a8d7aae98c6b545fd661
mister-storm <clfernando1979@hotmail.com>
2025-03-30 18:51:54 -0300

    refactoring tests cases to include assertAll and error mensage

--- src/test/java/br/com/misterstorm/codechallenge/ArraySumCheckerTest.java
+++ src/test/java/br/com/misterstorm/codechallenge/ArraySumCheckerTest.java
@@ -2,6 +2,7 @@ package br.com.misterstorm.codechallenge;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -12,8 +13,10 @@ class ArraySumCheckerTest {
         int[] arrayToCheck = { 2, 8, 6, 4, 10 };
         var sut  = new ArraySumChecker();
         var result = sut.check(arrayToCheck, 5);
-        assertEquals(-1, result[0]);
-        assertEquals(-1, result[1]);
+        assertAll(
+                () -> assertEquals(-1, result[0]),
+                () -> assertEquals(-1, result[1])
+        );
     }
 
     @Test
@@ -21,8 +24,10 @@ class ArraySumCheckerTest {
         int[] arrayToCheck = { 1, 4, 5, 9, 8, 16 };
         var sut  = new ArraySumChecker();
         var result = sut.check(arrayToCheck, 12);
-        assertEquals(1, result[0]);
-        assertEquals(4, result[1]);
+        assertAll(
+                () -> assertEquals(1, result[0], "error in the first element of the response array."),
+                () -> assertEquals(4, result[1], "error in the second element of the response array.")
+        );
     }
 
 }
