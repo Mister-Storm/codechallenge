ea45b69c2f65b0dfd509025c5769471293ee276e
mister-storm <clfernando1979@hotmail.com>
2025-03-30 19:33:11 -0300

    creating balanced brackets challenge

---
+++ src/test/java/br/com/misterstorm/codechallenge/BalancedBracketsTest.java
@@ -0,0 +1,29 @@
+package br.com.misterstorm.codechallenge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class BalancedBracketsTest {
+
+    @Test
+    void ensureThatReturnsFalseWhenExpressionIsNotBalanced() {
+        var notBalancedExpression = "{[(])}";
+
+        var sut = new BalancedBrackets();
+
+        assertFalse(sut.isBalanced(notBalancedExpression));
+    }
+
+    @Test
+    void ensureThatReturnsTrueWhenExpressionIsBalanced() {
+        var balancedExpression = "{{[[(())]]}}";
+
+        var sut = new BalancedBrackets();
+
+        assertTrue(sut.isBalanced(balancedExpression));
+    }
+
+}
